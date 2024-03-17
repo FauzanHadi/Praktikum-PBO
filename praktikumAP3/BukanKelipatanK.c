a5160f5e010d3897fed4ff42acedc4f97e793aee
FauzanHadi <163282150+FauzanHadi@users.noreply.github.com>
2024-03-17 21:16:18 +0700

    Update BukanKelipatanK.c

--- praktikumAP3/BukanKelipatanK.c
+++ praktikumAP3/BukanKelipatanK.c
@@ -4,24 +4,19 @@
 #include<stdio.h>
 
 int main(){
-    int k, n, i;
+    int k, n, i = 1, count =0;
    
     printf("Masukkan nilai k: ");
     scanf("%d", &k);
-
     printf("Masukkan nilai n: ");
     scanf("%d", &n);
 
-    int count =1;
-    if (n <= 0){
-        printf("Nilai n harus positif. ");
-    }
-    else {
-        for (i = 1; count <= n; i++){
-            if(i % k != 0);
-            {printf("%d", i);
-            count++;}
+    while(count < n){
+        if(i % k != 0){
+            printf("%d ", i);
+            count++;
         }
+        i++;
     }
 
     return 0;

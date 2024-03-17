1e868efd2e97f7db1cb5b8fc892aada05a01d2f3
FauzanHadi <163282150+FauzanHadi@users.noreply.github.com>
2024-03-17 21:20:48 +0700

    Create SegitigaUnik.c

---
+++ praktikumAP3/SegitigaUnik.c
@@ -0,0 +1,22 @@
+/* Pembuat   : Fauzan HADI*/
+/* NIM       : 24060123140176*/
+
+#include <stdio.h>
+
+int main() {
+    int n, angka = 0;
+    
+    printf("Masukkan n: ");
+    scanf("%d", &n);
+    
+    printf("Pola Segitiga\n");
+    for(int baris = 1; baris <= n; baris++) {
+        for(int kolom = 1; kolom <= baris; kolom++) {
+            printf("%d", angka);
+            angka = (angka + 1) % 10;
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}

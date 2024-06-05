933061d1cdd776668fa34808f10a0457740d97fe
FauzanHadi <163282150+FauzanHadi@users.noreply.github.com>
2024-06-05 20:31:14 +0700

    Add files via upload

---
+++ praktikumAP8/nomo1a.c
@@ -0,0 +1,29 @@
+// Pembuat  : Fauzan Hadi
+// NIM      : 24060123140176
+
+#include<stdio.h>
+
+int main(){
+    int i, X, N, IX;
+
+    int T[10] = {19 , 1, 28, 5, 20, 15, 52, 13, 16, 29};
+    N = 10;
+    X = 52;
+
+    T[N] = X;
+    i = 0;
+
+    while(T[i] != X){
+        i++;
+    }
+
+    if (i < N){
+        IX = i;
+    }
+    else{
+        IX = 0;
+    }
+
+    printf("%d", IX);
+    return 0;
+}
---
+++ praktikumAP8/nomor3b.c
@@ -0,0 +1,37 @@
+// Pembuat  : Fauzan Hadi
+// NIM      : 24060123140176
+
+#include<stdio.h>
+#include<stdbool.h>
+
+int main(){
+    int i, N, X, IX, atas, bawah, tengah;
+    bool found;
+    int T[13] = {1, 4, 6, 7, 10, 12, 18, 21, 29, 30, 41, 44, 47};
+
+    N = 13;
+    X = 8;
+    atas = 1;
+    bawah = N;
+    tengah = (atas + bawah)/2;
+
+    while((atas < bawah) && (X != T[tengah])){
+        if (X < T[tengah]){
+            bawah = tengah - 1;
+        }
+        else if (X > T[tengah]){
+            atas = tengah + 1;
+        }
+    tengah = (atas + bawah)/2;
+    }
+    if (X = T[tengah]) {
+        found = true;
+        IX = tengah;
+    } else {
+        found = false;
+        IX = 0;
+    }
+
+    printf("IX = %d\n", IX);
+    return 0;
+}
